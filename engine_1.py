3a2f5d75f04508bbd17bc811a45cd73d613a65df
SpaceHotDog <nicolassolignac@gmail.com>
2017-06-14 11:06:11 -0300

    Create engine_1.py

--- engine_1.py
+++ engine_1.py
@@ -1,8 +1,8 @@
 # Importamos la función create_engine
 from sqlalchemy import create_engine
 
-# Creamos un engine apuntando a la base de datos elegida. En este caso, census DB. 
-# El ejemplo utiliza una DB PostgreSQL alojada en la nube de Amazon.
+# Creamos un engine apuntando a la base de datos elegida. En este caso, 'census'. 
+# El ejemplo utiliza una DB PostgreSQL alojada en la cloud AmazonWS.
 engine = create_engine('postgresql+psycopg2://student:datacamp@postgresql.csrrinzqubik.us-east-1.rds.amazonaws.com:5432/census')
 
 # Usamos el método .table_names() en el engine para imprimir en pantalla los nombres de las tablas.
