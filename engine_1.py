efacf7812e1ab38a0f1286030e6623e23630f9b4
SpaceHotDog <nicolassolignac@gmail.com>
2017-06-14 09:35:13 -0300

    Create engine_1.py

--- engine_1.py
+++ engine_1.py
@@ -1,36 +1,8 @@
-from sqlalchemy import create_engine, MetaData, Table
-from Pandas import 
-from mathplotlib import 
+# Import create_engine function
+from sqlalchemy import create_engine
 
-#Create SQLAlchemy engine
-engine = create_engine ('Driver+Dialect:///Filename')
-metadata = MetaData()
+# Create an engine to the census database
+engine = create_engine('postgresql+psycopg2://student:datacamp@postgresql.csrrinzqubik.us-east-1.rds.amazonaws.com:5432/census')
 
-#Store the connection method
-connection = engine.connect()
-
-#Print out table names
+# Use the .table_names() method on the engine to print the table names
 print(engine.table_names())
-
-#Reflect the table 'TableName' and store metadata in 'tablePrint'
-tablePrint = Table('TableName', metadata, autoload=True, autoload_with=engine)
-
-#Print out metadata of 'tableName'
-print(repr('tablePrint'))
-
-# Print the column names
-print(tablePrint.columns.keys())
-
-# Print full table metadata
-print(repr(metadata.tables['TableName']))
-
-#SQLAlchemy querying. select = SELECT * FROM [TableName]
-stmt = select([TableName])
-print(stmt)
-
-#ResultProxy
-result_proxy = connection.execute(stmt)
-
-#ResultSet
-results = result_proxy.fetchall()
-
