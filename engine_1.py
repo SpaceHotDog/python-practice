1d4cdfeb1c2347eafe60b1bb0a9f4a4c3809472b
SpaceHotDog <nicolassolignac@gmail.com>
2017-07-19 20:00:07 -0300

    Update engine_1.py

--- engine_1.py
+++ engine_1.py
@@ -3,10 +3,11 @@ from sqlalchemy import create_engine
 
 # Creamos un engine apuntando a la base de datos elegida. En este caso, 'census'. 
 # El ejemplo utiliza una DB PostgreSQL alojada en AWS.
+#Mas info en http://docs.sqlalchemy.org/en/latest/core/engines.html
 engine = create_engine('postgresql+psycopg2://student:datacamp@postgresql.csrrinzqubik.us-east-1.rds.amazonaws.com:5432/census')
 
 #Nos conectamos a la base de datos.
 connection = engine.connect()
 
-# Usamos el método .table_names() sobre el engine para guardar los nombres de las tablas en la variable 'var'.
+# Usamos el método table_names sobre el engine para obtener los nombres de las tablas, y luuego las guardamos en la variable 'var'.
 var = print(engine.table_names())
