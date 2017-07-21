163f5ca4db6367915e3c6e7474e7284ec6d4a2a1
SpaceHotDog <nicolassolignac@gmail.com>
2017-07-21 12:36:59 -0300

    Update engine_1.py

--- engine_1.py
+++ engine_1.py
@@ -1,13 +1,18 @@
 # Importamos dependencias.
-from sqlalchemy import create_engine
+from sqlalchemy import *
 
-# Creamos un engine apuntando a la base de datos elegida. En este caso, 'census'. 
-# El ejemplo utiliza una DB PostgreSQL alojada en AWS.
-#Mas info en http://docs.sqlalchemy.org/en/latest/core/engines.html
-engine = create_engine('postgresql+psycopg2://student:datacamp@postgresql.csrrinzqubik.us-east-1.rds.amazonaws.com:5432/census')
+# Creamos una variable #engine, que contiene el metodo de conexion del objeto Engine.
+#Mas info en docs.sqlalchemy.org/en/latest/core/engines.html
+engine = create_engine('lenguaje+driver://usuario:contraseña@host:puerto/nombreDB')
 
-#Nos conectamos a la base de datos.
+#Definimos la variable que contiene el metodo de conexion.
 connection = engine.connect()
 
-# Usamos el método table_names sobre el engine para obtener los nombres de las tablas, y luuego las guardamos en la variable 'var'.
+# Usamos el método table_names sobre el engine para obtener los nombres de las tablas, y luego las guardamos en la variable 'var'.
 var = print(engine.table_names())
+
+#Inicializamos la variable #meta donde se guarda la metadata.
+meta = MetaData()
+
+#Guardamos los nombres de las columnas en la variable #messages.
+messages = Table('messages', meta, autoload=True, autoload_with=engine)
