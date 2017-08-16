6c27725d709ff52ddfa7d11f54b3e40a2a441cc9
SpaceHotDog <nicolassolignac@gmail.com>
2017-08-16 09:46:11 -0300

    Update and rename importCSV.py to import.py

---
+++ import.py
@@ -0,0 +1,18 @@
+def contQuest = input("Desea importar para analisis? Y/N: ")
+if contQuest == Y:
+  raise NameError
+continue
+
+  def importar_arc (file):
+    dataSet = file.csv
+  
+      try:
+        readOnly = open("dataSet", "r")
+ 
+      except NameError:
+        Print("Mal nombre o ext?")
+  
+      finally:
+        Print(readOnly)
+        readOnly.close()  
+  
